77f5dc556728130ec1220610b0926d5613ac1e02
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-05-21 10:27:19 -0300

    feat(mobile): redirecting to memories page if the user is already authenticated

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -13,15 +13,27 @@ import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
+import { useEffect, useState } from 'react'
+import * as SecureStore from 'expo-secure-store'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) return <SplashScreen />
 
@@ -39,7 +51,10 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
