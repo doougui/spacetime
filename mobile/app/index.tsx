02ce59ad6e81095293b3c22cb50c6de1628f9d83
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-05-21 10:23:06 -0300

    feat(mobile): creating routes layout

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,25 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import { styled } from 'nativewind'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { useCallback, useEffect } from 'react'
-import * as SecureStore from 'expo-secure-store'
 import { useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
+import { useCallback, useEffect } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
 
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { api } from '../src/lib/api'
-
-const StyledStripes = styled(Stripes)
 
 // Endpoint
 const discovery = {
@@ -31,12 +17,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -71,19 +51,8 @@ export default function App() {
     }
   }, [response, handleGithubOAuthCode])
 
-  if (!hasLoadedFonts) return null
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 bg-gray-900 px-8 py-10"
-      imageStyle={{
-        position: 'absolute',
-        left: '-100%',
-      }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -113,8 +82,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
